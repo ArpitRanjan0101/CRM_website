cba18d963494a4de5f867651f58c463dcb0ff718
sshukla289 <ptsshukla9@gmail.com>
2026-04-23 11:42:26 +0530

    gitignore files

--- .next/dev/static/chunks/app_page_02gwhbi.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/components_02kathm._.js","static/chunks/node_modules_next_04sijip._.js"],
-    source: "dynamic"
-});
